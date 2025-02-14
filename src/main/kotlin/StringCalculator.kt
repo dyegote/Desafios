893cfe92af8d888f27630db92ce0bddf26186248
Diego Tobar <dtobar@veritran.com>
2025-02-13 19:53:31 -0500

    String calculator Step 1: int add(string numbers), create function and unit test

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -1,4 +1,18 @@
 package org.example
 
 class StringCalculator {
+
+    fun add(numbers: String): Int {
+        val sum: Int
+        if(numbers == "") {
+            sum = 0
+        } else {
+            val numbersList: List<Int> = numbers.split(',').map { it.toInt() }
+            if(numbersList.size == 1)
+                sum = numbersList[0]
+            else
+                sum = numbersList[0] + numbersList[1]
+        }
+        return sum
+    }
 }
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -1,3 +1,29 @@
+import org.example.StringCalculator
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class StringCalculatorTest
+class StringCalculatorTest {
+
+    val stringCalculator: StringCalculator by lazy { StringCalculator() }
+
+    @Test
+    fun `GIVEN string number empty WHEN Call add THEN result is 0`() {
+        val stringNumbers = ""
+        val sum = stringCalculator.add(stringNumbers)
+        assertEquals(0, sum)
+    }
+
+    @Test
+    fun `GIVEN string number is 5 WHEN Call add THEN result is 5`() {
+        val stringNumbers = "5"
+        val sum = stringCalculator.add(stringNumbers)
+        assertEquals(5, sum)
+    }
+
+    @Test
+    fun `GIVEN string number is 3,6 WHEN Call add THEN result is 9`() {
+        val stringNumbers = "3,6"
+        val sum = stringCalculator.add(stringNumbers)
+        assertEquals(9, sum)
+    }
+}
