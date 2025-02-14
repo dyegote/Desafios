9af99c360219511dc4ae32395bc062175ebe8be8
Diego Tobar <dtobar@veritran.com>
2025-02-13 20:38:21 -0500

    Refactor add function

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -3,15 +3,13 @@ package org.example
 class StringCalculator {
 
     fun add(numbers: String): Int {
-        val sum: Int
-        if(numbers == "") {
-            sum = 0
-        } else {
-            val numbersList: List<Int> = numbers.split(',').map { it.toInt() }
-            if(numbersList.size == 1)
-                sum = numbersList[0]
-            else
-                sum = numbersList[0] + numbersList[1]
+        var sum = 0
+        if(numbers != "") {
+            val numbersMap = HashMap<Int, Int>()
+            numbers.split(',').mapIndexed { index, num ->
+                numbersMap.put(index + 1, num.toInt())
+            }
+            sum = (numbersMap[1] ?: 0) + (numbersMap[2] ?: 0)
         }
         return sum
     }
